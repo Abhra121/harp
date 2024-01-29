4d28c3a499cc830d21ec972d94d0d5bbe1bcf822
Abhra Adhikari <33281900+Abhra121@users.noreply.github.com>
2024-01-29 11:29:43 +0530

    Update store.php

--- web/store.php
+++ web/store.php
@@ -1,22 +1,51 @@
 <?php
-if (isset($_GET["SSID"]))
+global $a;
+if (isset($_GET["SSID"]) && isset($_GET["module"]))
 {
 	
         $ssid=$_GET["SSID"];
-        $command = escapeshellcmd('python3 /var/www/html/test.py '.$ssid);
-        $output = exec($command);
-        echo $output;
+        $password=$_GET["module"];
+	if($password =='cavili') {
+           $a=1;
+           echo "<br />";
+           echo "Selected module type cavili";
+           $command = escapeshellcmd('python3 /var/www/html/test.py '.$ssid);
+           $output = exec($command);
+           echo $output;
+           
+        }else if($password =='simcom') {
+           $a=1;
+           
+           echo "<br />";
+           echo "Selected module type simcom";
+           //$command = escapeshellcmd('python3 /var/www/html/test.py '.$ssid);
+           //$output = exec($command);
+           //echo $output;
+           
+        }else{
+           $a=0;
+           echo "<br />";
+           //echo "<br />";
+           echo "Enter module name correctly";
+           //echo "<br />";
+           //echo "Failed to update APN";
+           //echo '<a href="Gsm.html"><br /><br />Return to Previous Page</a>';
+        }     
+        //$command = escapeshellcmd('python3 /var/www/html/test.py '.$ssid);
+        //$output = exec($command);
+        //echo $output;
         
-	
-        if($return !=0) {
+	if($return !=0 || $a !=1) {
+        //if($return !=0) {
+           echo "<br />";
            echo "<br />";
            echo "Failed to update APN";
-           echo '<a href="Wlan.html"><br /><br />Return to Previous Page</a>';
+           echo '<a href="Gsm.html"><br /><br />Return to Previous Page</a>';
         }else{
            echo "<br />";
            echo "<br />";
            echo "Sucessfully updated APN";
-           echo '<a href="Wlan.html"><br /><br />Return to Modem Setup Page</a>';
+           echo '<a href="Gsm.html"><br /><br />Return to Modem Setup Page</a>';
         }
         
 } ?>
