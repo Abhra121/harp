f8e96999e170159ca6260cb30211bbe6f05088c5
Abhra Adhikari <33281900+Abhra121@users.noreply.github.com>
2024-01-29 11:36:24 +0530

    Update test1.py

--- web/test1.py
+++ web/test1.py
@@ -2,36 +2,6 @@
 #!/usr/bin/env python3
 
 
-#import os
-#import time
-
-#import sys
- 
-# total arguments
-#n = len(sys.argv)
-
-#print("\nAPN Passed:", end = " ")
-
-#s=""
-#s=(sys.argv[1])
-
-#print(s)
-
-#s=s+'"'
-
-
-#with open("/home/pi/apn.txt") as f:
-#    lines = f.readlines() #read.
-#modify.
-#    lines[1] = 'connect "/usr/sbin/chat -v -f /etc/chatscripts/gprs -T ' +s+  '\n' #you can repla>
-
-#with open("/home/pi/apn.txt", "w") as f:
-#    f.writelines(lines) #write back.
-
-
-#os.popen('sudo cp /home/pi/apn.txt /etc/ppp/peers/rnet')
-
-
 import sys
 import os
 import subprocess
@@ -46,9 +16,6 @@ n = len(sys.argv)
 print("\nAPN Passed:", end = " ")
 #print("APN is:\n")
 
-#for i in range(1, n):
-#    print(sys.argv[i], end = " ")
-     
 # Addition of numbers
 Sum = 0
 # Using argparse module
@@ -61,15 +28,6 @@ s=""
 s=(sys.argv[1])
 
 print(s)
-#s=s+"'"
-
-#x= 'python /home/pi/2.py '+s
-#print(x)
-
-#os.popen('python /home/pi/2.py airtelgprs')
-
-#time.sleep(1)
-#exit
 
 s='"'+s+'"'
 print(s)
